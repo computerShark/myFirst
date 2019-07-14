491cc2c6037748b70861c7c09a44bdc11197cff3
Abdel Rahman Barghout <30629701+abarghou1993@users.noreply.github.com>
2019-07-13 21:30:49 -0400

    This is the first java code
    This code adds two variables a and b and stores result in c

---
+++ add.java
@@ -0,0 +1,12 @@
+package add;
+public class Add 
+{
+ public static void main(String[] args) 
+ {
+       /************************
+       Add a and b, then put the result in c.
+       a = 4, b = 5 
+       ************************/
+ }// end of main
+    
+}// end of Add class
