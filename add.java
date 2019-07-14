e000c3c9060b11b52f44d6bef85014e4a5ba20a9
computerShark <52873058+computerShark@users.noreply.github.com>
2019-07-13 23:46:27 -0400

    Update add.java

--- add.java
+++ add.java
@@ -7,6 +7,9 @@ public class Add
        Add a and b, then put the result in c.
        a = 4, b = 5 
        ************************/
+int a = 4;
+  int b = 5;
+  c = a + b;
  }// end of main
     
 }// end of Add class
